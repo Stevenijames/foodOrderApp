11b561cf6b97255e6d13c71ed91ada76aed7b214
Stevenijames <ijamesproductions1@gmail.com>
2022-09-28 08:45:26 -0600

    updates to app-wide state management

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,13 +2,16 @@ import { useContext } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 
+//1.import CartContext
 import CartContext from "../../store/CartContext";
 
 import CartIcon from "../Cart/CartIcon";
 
+//passing in CartContext - useContext(CartContext);
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
 
+  //Takes an array and turns it into a single value
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,53 @@
+import { useReducer } from "react";
+
 import CartContext from "./CartContext";
 
-//Manages CartContext Data and provides that Context to any components that want acces to it.
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+//Manages CartContext Data and provides that Context to any components that want access to it.
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   //Concrete Context Value which will be updated over time
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
   return (
+    //make sure to add {props.children}
     //add value pointer to CartContext.Provider
+    //a wrapper that gives any component wrapped within access to context
+    //replace App.js Fragment with CartProvider component
+    //also for useContext() pass (CartContext) in HeaderCartButton.js
     <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
